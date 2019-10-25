bf6738926c991e961e6aee1af04512de7be47ce2
Maduabughichi Achilefu <aachilefu@yahoo.com>
2019-10-25 14:44:12 +0100

    Initial commit

--- app/src/main/java/com/alc/android/activityanimations/ActivityTwo.java
+++ app/src/main/java/com/alc/android/activityanimations/ActivityTwo.java
@@ -16,6 +16,7 @@ public class ActivityTwo extends AppCompatActivity {
     public static final String EXTRA_CIRCULAR_REVEAL_X = "EXTRA_CIRCULAR_REVEAL_X";
     public static final String EXTRA_CIRCULAR_REVEAL_Y = "EXTRA_CIRCULAR_REVEAL_Y";
     View rootLayout;
+
     private int revealX;
     private int revealY;
     @Override
